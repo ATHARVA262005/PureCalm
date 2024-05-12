1adac2bac4de16de7c5e994ee05c440963caddc0
Atharva262005 <atharvaralegankar2005@gmail.com>
2024-05-12 15:26:03 +0530

    changes in aboutus,contactus,footer

--- src/Componennts/footer/Footer.jsx
+++ src/Componennts/footer/Footer.jsx
@@ -7,19 +7,19 @@ const MyFooter = () => {
       <footer className="py-4 my-0 " style={{ backgroundColor: '#198754', color: 'white' }}>
         <Nav className="justify-content-center border-bottom pb-3 mb-3">
           <Nav.Item>
-          <Nav.Link><NavLink to="/home" className="px-2 text-white">Home</NavLink></Nav.Link>
+          <Nav.Link><NavLink  to="/home" className="px-2 text-white navlink">Home</NavLink></Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link><NavLink to="/therepies" className="px-2 text-white">Therepies</NavLink></Nav.Link>
+            <Nav.Link><NavLink to="/therepies" className="px-2 text-white navlink">Therepies</NavLink></Nav.Link>
           </Nav.Item>
           <Nav.Item>
-          <Nav.Link><NavLink to="/community" className="px-2 text-white">Community</NavLink></Nav.Link>
+          <Nav.Link><NavLink to="/community" className="px-2 text-white navlink">Community</NavLink></Nav.Link>
           </Nav.Item>
           <Nav.Item>
-          <Nav.Link><NavLink to="/about" className="px-2 text-white">About Us</NavLink></Nav.Link>
+          <Nav.Link><NavLink to="/about" className="px-2 text-white navlink">About Us</NavLink></Nav.Link>
           </Nav.Item>
           <Nav.Item>
-          <Nav.Link><NavLink to="/contact" className="px-2 text-white">Contact Us</NavLink></Nav.Link>
+          <Nav.Link><NavLink to="/contact" className="px-2 text-white navlink">Contact Us</NavLink></Nav.Link>
           </Nav.Item>
         </Nav>
         <p className="text-center">© 2024 PureCalm</p>
